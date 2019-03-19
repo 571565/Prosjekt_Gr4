2de2e7bb1c135ebceb7709f8541f568e34e06516
571565 <571565@stud.hvl.no>
2019-03-19 13:16:38 +0100

    Startkode

---
+++ DAT109_Prosjekt_Gr4/src/no/hvl/dat109/Bruker.java
@@ -0,0 +1,24 @@
+package no.hvl.dat109;
+
+public class Bruker {
+	
+
+	private Integer tlf;
+	
+	
+	public Bruker (Integer tlf) {
+		this.tlf = tlf;
+	}
+
+	public Integer getTlf() {
+		return tlf;
+	}
+
+	public void setTlf(Integer tlf) {
+		this.tlf = tlf;
+	}
+	
+	
+	
+
+}
